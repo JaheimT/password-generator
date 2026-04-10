7d4729a2191093caf4bbb303a2384c3bb7aac753
Jaheim Thompson <113160062+JaheimT@users.noreply.github.com>
2026-04-10 09:39:25 +0100

    Final rendition
    will come back in the future to improve the logic

--- password-generator.py
+++ password-generator.py
@@ -8,20 +8,38 @@ Password = ""
 CharP = "qwertyuiopasdfghjklzxcvbnm"
 # user picks desired lenght of password
 length = int(input("what length do u require -   " ))
+New_length = 0
 # if the user says yes numbers are added to the character pool
 numbers_choice = input("Include numbers? (y/n): ")
 if numbers_choice == "y":
-    CharP = CharP + string.digits + "A"
+   # removes 1 unit from the required lenght of the password
+   New_length = New_length - 1
+   CharP = CharP + string.digits 
+   for x in range(1):
+       random_num = random.choice(string.digits)
+       Password += random_num
 # if the user says yes Uppercase letters are added to the character pool
 uppercase_choice = input("Include uppercase? (y/n): ")
 if uppercase_choice == "y":
+   New_length = New_length - 1
    CharP = CharP + string.ascii_uppercase
+   for x in range(1):
+       random_upper = random.choice(string.ascii_uppercase)
+       Password += random_upper
 # if the user says yes symbols are added to the character pool
-punctuation_choice = input("Include symbols? (y/n)?")
+punctuation_choice = input("Include symbols? (y/n): ")
 if punctuation_choice == "y":
+   New_length = New_length - 1
    CharP = CharP + string.punctuation
+   for x in range(1):
+       random_pun = random.choice(string.punctuation)
+       Password += random_pun
 # pickes random characters from the pool and creates the password
-for x in range(length):
+for x in range(length + New_length):
    random_char = random.choice(CharP)
    Password += random_char
+# turn the string into a list to help shuffle the characters for the password around
+password_list = list(Password)
+random.shuffle(password_list)
+Password = "".join(password_list)
 print (Password)
